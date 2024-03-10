5be25e5d5c0a8234e55b2d31fc208444b55f346a
tahirshaikh099 <57958180+tahirshaikh099@users.noreply.github.com>
2024-03-10 14:56:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const { StatusCodes } = require("http-status-codes");
+const bodyParser = require('body-parser');
+const connectDB = require("./db/Connect.js");
+const tradeRoutes = require('./routes/Router.js');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api', tradeRoutes);
+
+
+app.use('/', (req, res) => {
+    res.status(StatusCodes.NOT_FOUND).json({ status: false, message: 'Sorry, Endpoint does not exist!' });
+});
+
+
+app.use((err, req, res, next) => {
+    if (err) {
+        res.status(err.statusCode || err.status || StatusCodes.INTERNAL_SERVER_ERROR).send(err.data || err.message || {});
+    } else {
+        next();
+    }
+});
+
+const port = process.env.PORT || 5000;
+
+/**
+ * Asynchronous function to start the server after connecting to the database.
+ *
+ * @return {Promise<void>} Promise that resolves after the server has started
+ */
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGODB_URI);
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (error) {
+        console.log("error =>", error);
+    }
+}
+
+start();
