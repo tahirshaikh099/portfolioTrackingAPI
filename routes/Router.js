544da4cc97e76ee895484881b2c4814a4f108edd
tahirshaikh099 <57958180+tahirshaikh099@users.noreply.github.com>
2024-03-10 14:57:23 +0530

    Add files via upload

---
+++ routes/Router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+
+// Importing controller function for each route
+const {
+    addTrade,
+    addStock,
+    deleteTrade,
+    getApiKey,
+    getCumulativeReturn,
+    getStocks,
+    getHoldings,
+    getPortfolio,
+    modifyTrade } = require('../controller/Controller.js');
+
+// HTTP GET calls
+router.route('/portfolio').get(getPortfolio);
+router.route('/holdings').get(getHoldings);
+router.route('/returns').get(getCumulativeReturn);
+router.route('/getStocks').get(getStocks);
+router.route('/getApiKey').get(getApiKey);
+
+// HTTP POST calls
+router.route('/addTrade').post(addTrade);
+router.route('/addStocks').post(addStock);
+
+// HTTP PUT calls
+router.route('/updateTrade').put(modifyTrade);
+
+// HTTP DELETE calls
+router.route('/removeTrade').delete(deleteTrade);
+
+
+
+module.exports = router;
